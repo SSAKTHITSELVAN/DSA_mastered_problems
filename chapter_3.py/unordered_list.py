8031f4beae08a02b5abec6edf6135c9742119560
SakthiSelvan <ssakthitselvan7@gmail.com>
2025-03-22 07:38:46 +0530

    Updated feature in filename.py

--- chapter_3.py/unordered_list.py
+++ chapter_3.py/unordered_list.py
@@ -4,7 +4,7 @@ class Node:
     def __init__(self, node_data):
         """Initialization of nodes"""
         self._data = node_data
-        self._next = None  # Default next reference to None
+        self._next = None
     
     @property
     def data(self):
@@ -25,3 +25,70 @@ class Node:
     def next(self, next_node):
         """Next reference setter"""
         self._next = next_node
+
+class Unordered_List:
+    """Represents the unordered list"""
+    def __init__(self):
+        self.head = None
+        self.size = 0
+    
+    def add(self, item):
+        temp = Node(item)
+        temp.next = self.head
+        self.head = temp
+        self.size += 1
+    
+    def is_empty(self):
+        return not bool(self.head)
+    
+    def length(self):
+        return self.size
+    
+    def search(self, target):
+        "simple linear search"
+        temp = self.head
+        while temp:
+            if temp.data == target:
+                return True
+            temp = temp.next
+        return False
+    
+    def show_list_items(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+    
+    def remove(self, item):
+        """remove the item"""
+        previous = None
+        temp = self.head
+        
+        while temp:
+            if temp.data == item:
+                break
+            previous = temp
+            temp = temp.next
+        
+        if temp is None:
+            raise ValueError(f"Element -- {item} -- not found...")
+
+        if previous is None:
+            self.head = temp.next
+        else:
+            previous.next = temp.next
+        
+        self.size -= 1
+
+# Testing the corrected code
+a = Unordered_List()
+a.add(5)
+a.add(2)
+a.add(3)
+a.show_list_items()
+print("size  ",a.length())
+a.remove(2)
+print("size  ",a.length())
+a.show_list_items()
+print(a.search(5))
